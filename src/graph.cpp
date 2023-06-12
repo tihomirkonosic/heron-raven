ca730f684ba588d3ce6fc4e441758fa3c74f6d4b
FilipTomas <filipnash32@gmail.com>
2023-06-12 10:43:37 +0000

    fixed error in last commit

--- src/graph.cpp
+++ src/graph.cpp
@@ -111,6 +111,136 @@ Graph::Node::Node(Node* begin, Node* end)
       data);
   }
 
+Graph::Node::Node(Node* begin, Node* end, bool is_unitig)
+    : id(num_objects++),
+      sequence(),
+      count(),
+      is_unitig(is_unitig),
+      is_circular(false),
+      is_polished(),
+      transitive(),
+      front_inedges(),
+      front_outedges(),
+      back_inedges(),
+      back_outedges(),
+      pair(),
+      unitig_nodes() {
+    std::string data{};
+
+
+  auto it = begin;
+  if(begin == end){
+
+      // if(it->outdegree() > 0){
+      //   data += it->outedges.front()->Label();
+        
+      // }
+      // if(it->indegree() > 0){
+      //   data += it->inedges.front()->Label();
+        
+      // }
+      data += it->sequence.InflateData();
+      count += it->count;
+      unitig_nodes.emplace_back(it);
+  }
+  else{
+    while (true) {
+      data += it->outedges.front()->Label();
+      count += it->count;
+      unitig_nodes.emplace_back(it);
+      if ((it = it->outedges.front()->head) == end) {
+        unitig_nodes.emplace_back(it);
+        break;
+        }
+      }
+    
+      if (begin != end) {
+        data += end->sequence.InflateData();
+        count += end->count;
+      }
+  }
+
+
+    std::uint32_t front_inedge_count{0};
+    std::uint32_t back_inedge_count{0};
+    std::uint32_t front_outedge_count{0};
+    std::uint32_t back_outedge_count{0};
+
+    for(auto& inedge : begin->inedges){
+      if(std::find(unitig_nodes.begin(), unitig_nodes.end(), inedge->tail) == unitig_nodes.end()){
+        front_inedges.emplace_back(inedge);
+        front_inedge_count++;
+      }
+    };
+
+    for(auto& outedge : begin->outedges){
+      if(std::find(unitig_nodes.begin(), unitig_nodes.end(), outedge->head) == unitig_nodes.end()){
+        front_outedges.emplace_back(outedge);
+        front_outedge_count++;
+      }
+    };
+    
+    for(auto& inedge : end->inedges){
+      if(std::find(unitig_nodes.begin(), unitig_nodes.end(), inedge->tail) == unitig_nodes.end()){      
+        back_inedges.emplace_back(inedge);
+        back_inedge_count++;
+      };
+    };
+
+
+    for(auto& outedge : end->outedges){
+      if(std::find(unitig_nodes.begin(), unitig_nodes.end(), outedge->head) == unitig_nodes.end()){      
+        back_outedges.emplace_back(outedge);
+        back_outedge_count++;
+      };
+    };
+    sequence = biosoup::NucleicAcid(
+      "Utg" + std::to_string(id & (~1UL)),
+      data);
+  }
+
+  // Graph::Node::Node(Node* begin, Node* end, bool is_unitig, std::uint32_t id)
+  //   : id(id),
+  //     sequence(),
+  //     count(),
+  //     is_unitig(is_unitig),
+  //     is_circular(begin == end),
+  //     is_polished(),
+  //     transitive(),
+  //     inedges(),
+  //     outedges(),
+  //     pair(),
+  //     unitig_nodes() {
+
+  //   std::string data{};
+
+  //   auto it = begin;
+  //   while (true) {
+  //     data += it->outedges.front()->Label();
+  //     count += it->count;
+  //     //unitig_nodes.emplace_back(it->id);
+  //     if ((it = it->outedges.front()->head) == end) {
+  //       break;
+  //     }
+  //   }
+  //   if (begin != end) {
+  //     data += end->sequence.InflateData();
+  //     count += end->count;
+  //   }
+
+  //   for(auto& inedge : begin->inedges){
+  //     inedges.emplace_back(inedge);
+  //   };
+
+  //   for(auto& outedge : end->outedges){
+  //     outedges.emplace_back(outedge);
+  //   };
+
+  //   sequence = biosoup::NucleicAcid(
+  //     "Utg" + std::to_string(id & (~1UL)),
+  //     data);
+  // }
+
 Graph::Edge::Edge(Node* tail, Node* head, std::uint32_t length)
     : id(num_objects++),
       length(length),
